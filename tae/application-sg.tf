3189f3c4f363926cb944d1992c611a5c79bbabcf
Taeyang <xodid3446@gmail.com>
2024-04-17 15:00:52 +0900

    240417

--- tae/application-sg.tf
+++ tae/application-sg.tf
@@ -22,7 +22,7 @@ resource "azurerm_application_security_group" "db" {
   location            = azurerm_resource_group.rg.location
 }
 
-resource "azurerm_application_security_group" "exl_lb" {
+resource "azurerm_application_security_group" "ext_lb" {
   name                = "${var.project_name_prefix}-WAS-ASG"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
