532c0ae98a24e1e43f9005242349d0395e3d9bc2
Taeyang <xodid3446@gmail.com>
2024-04-17 15:32:09 +0900

    240417

--- tae/application-sg.tf
+++ tae/application-sg.tf
@@ -17,43 +17,43 @@ resource "azurerm_application_security_group" "was" {
 }
 
 resource "azurerm_application_security_group" "db" {
-  name                = "${var.project_name_prefix}-WAS-ASG"
+  name                = "${var.project_name_prefix}-DS-ASG"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
 resource "azurerm_application_security_group" "ext_lb" {
-  name                = "${var.project_name_prefix}-WAS-ASG"
+  name                = "${var.project_name_prefix}-ELB-ASG"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
 resource "azurerm_application_security_group" "int_lb" {
-  name                = "${var.project_name_prefix}-WAS-ASG"
+  name                = "${var.project_name_prefix}-ILB-ASG"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
 resource "azurerm_application_security_group" "web_efs" {
-  name                = "${var.project_name_prefix}-WAS-ASG"
+  name                = "${var.project_name_prefix}-Web-EFS-ASG"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
 resource "azurerm_application_security_group" "was_efs" {
-  name                = "${var.project_name_prefix}-WAS-ASG"
+  name                = "${var.project_name_prefix}-WAS-EFS-ASG"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
 resource "azurerm_application_security_group" "cluster" {
-  name                = "${var.project_name_prefix}-WAS-ASG"
+  name                = "${var.project_name_prefix}-Cluster-ASG"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
-resource "azurerm_application_security_group" "ccontrol_plane" {
-  name                = "${var.project_name_prefix}-WAS-ASG"
+resource "azurerm_application_security_group" "control_plane" {
+  name                = "${var.project_name_prefix}-Control-Plane-ASG"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
