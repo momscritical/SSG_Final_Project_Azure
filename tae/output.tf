af3ecf404d9acafd7873e26b4fc15b1dedbb6d74
Taeyang <xodid3446@gmail.com>
2024-04-18 15:06:36 +0900

    240418 Testing AKS

--- tae/output.tf
+++ tae/output.tf
@@ -7,12 +7,12 @@ output "public_ip_address" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  value     = azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.cluster.kube_config_raw
 
   sensitive = true
 }
