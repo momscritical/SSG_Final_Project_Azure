247c16552f41f0174a5dba5739705d94dfe5d1df
Taeyang <xodid3446@gmail.com>
2024-04-23 16:01:12 +0900

    Add Internal-LB

--- tae/aks-cluster.tf
+++ tae/aks-cluster.tf
@@ -1,38 +1,38 @@
-resource "azurerm_kubernetes_cluster" "cluster" {
-  location            = azurerm_resource_group.rg.location
-  name                = "${var.project_name_prefix}-AKS-Cluster"
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "${var.project_name_prefix}-AKS-Cluster"
-  kubernetes_version =  "1.29"  
+# resource "azurerm_kubernetes_cluster" "cluster" {
+#   location            = azurerm_resource_group.rg.location
+#   name                = "${var.project_name_prefix}-AKS-Cluster"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   dns_prefix          = "${var.project_name_prefix}-AKS-Cluster"
+#   kubernetes_version =  "1.29"  
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  default_node_pool {
-    name       = "default"
-    vm_size    = "standard_d2as_v4"
-    node_count = 1
-    enable_auto_scaling = false
-  }
+#   default_node_pool {
+#     name       = "default"
+#     vm_size    = "standard_d2as_v4"
+#     node_count = 1
+#     enable_auto_scaling = false
+#   }
 
-  linux_profile {
-    admin_username = var.username
+#   linux_profile {
+#     admin_username = var.username
 
-    ssh_key {
-      key_data = file("${var.public_key_location}")
-    }
-  }
-  network_profile {
-    network_plugin    = "kubenet"
-    network_policy     = "calico" 
-    load_balancer_sku = "standard"
-    # pod_cidr       = "10.0.0.0/24"
-    # service_cidr   = "10.240.0.0/24"
-    # dns_service_ip = "10.240.0.10"
-  }
+#     ssh_key {
+#       key_data = file("${var.public_key_location}")
+#     }
+#   }
+#   network_profile {
+#     network_plugin    = "kubenet"
+#     network_policy     = "calico" 
+#     load_balancer_sku = "standard"
+#     # pod_cidr       = "10.0.0.0/24"
+#     # service_cidr   = "10.240.0.0/24"
+#     # dns_service_ip = "10.240.0.10"
+#   }
 
-  tags = {
-    Name = "${var.project_name_prefix}-Cluster"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name_prefix}-Cluster"
+#   }
+# }
--- tae/aks-node.tf
+++ tae/aks-node.tf
@@ -1,75 +1,83 @@
-locals {
-  node_pools = {
-    "web-01" = {
-      name       = "web01"
-      # subnet_ids = azurerm_subnet.web[0].id
-      taints     = ["web=true:NoSchedule"]
-      asg_id     = azurerm_application_security_group.web.id
-      tags = {
-        Name        = "Web-Node"
-        Environment = "production"
-        ASG-Tag     = "Web-Node"
-      }
-    }
-    "web-02" = {
-      name       = "web02"
-      # subnet_ids = azurerm_subnet.was[1].id
-      taints     = ["was=true:NoSchedule"]
-      asg_id     = azurerm_application_security_group.web.id
-      tags = {
-        Name        = "Web-Node"
-        Environment = "production"
-        ASG-Tag     = "Web-Node"
-      }
-    }
-    "was-01" = {
-      name       = "was01"
-      # subnet_ids = azurerm_subnet.was[0].id
-      taints     = ["web=true:NoSchedule"]
-      asg_id     = azurerm_application_security_group.was.id
-      tags = {
-        Name        = "WAS-Node"
-        Environment = "production"
-        ASG-Tag     = "WAS-Node"
-      }
-    }
-    "was-02" = {
-      name       = "was02"
-      # subnet_ids = azurerm_subnet.was[1].id
-      taints     = ["was=true:NoSchedule"]
-      asg_id     = azurerm_application_security_group.was.id
-      tags = {
-        Name        = "WAS-Node"
-        Environment = "production"
-        ASG-Tag     = "WAS-Node"
-      }
-    }
-  }
-}
+# locals {
+#   node_pools = {
+#     "web-01" = {
+#       name       = "web01"
+#       # subnet_ids = azurerm_subnet.web[0].id
+#       taints     = ["web=true:NoSchedule"]
+#       asg_id     = azurerm_application_security_group.web.id
+#       tags = {
+#         Name        = "Web-Node"
+#         Environment = "production"
+#         ASG-Tag     = "Web-Node"
+#       }
+#     }
+#     "web-02" = {
+#       name       = "web02"
+#       # subnet_ids = azurerm_subnet.was[1].id
+#       taints     = ["was=true:NoSchedule"]
+#       asg_id     = azurerm_application_security_group.web.id
+#       tags = {
+#         Name        = "Web-Node"
+#         Environment = "production"
+#         ASG-Tag     = "Web-Node"
+#       }
+#     }
+#     "was-01" = {
+#       name       = "was01"
+#       # subnet_ids = azurerm_subnet.was[0].id
+#       taints     = ["web=true:NoSchedule"]
+#       asg_id     = azurerm_application_security_group.was.id
+#       tags = {
+#         Name        = "WAS-Node"
+#         Environment = "production"
+#         ASG-Tag     = "WAS-Node"
+#       }
+#     }
+#     "was-02" = {
+#       name       = "was02"
+#       # subnet_ids = azurerm_subnet.was[1].id
+#       taints     = ["was=true:NoSchedule"]
+#       asg_id     = azurerm_application_security_group.was.id
+#       tags = {
+#         Name        = "WAS-Node"
+#         Environment = "production"
+#         ASG-Tag     = "WAS-Node"
+#       }
+#     }
+#   }
+# }
 
-resource "azurerm_kubernetes_cluster_node_pool" "node_pools" {
-  for_each = local.node_pools
+# resource "azurerm_kubernetes_cluster_node_pool" "node_pools" {
+#   for_each = local.node_pools
 
-  name                  = each.value.name
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster.id
-  vm_size               = "standard_d2as_v4"
-  enable_node_public_ip = false
-  enable_auto_scaling   = true
-  scale_down_mode       = "Delete"
-  node_count            = 1
-  max_count             = 2
-  min_count             = 1
-  node_taints           = each.value.taints
-  # vnet_subnet_id        = each.value.subnet_ids
-  # pod_subnet_id = each.value.subnet_ids
+#   name                  = each.value.name
+#   kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster.id
+#   vm_size               = "standard_d2as_v4"
+#   enable_node_public_ip = false
+#   enable_auto_scaling   = true
+#   scale_down_mode       = "Delete"
+#   node_count            = 1
+#   max_count             = 2
+#   min_count             = 1
+#   node_taints           = each.value.taints
+#   # vnet_subnet_id        = each.value.subnet_ids
+#   # pod_subnet_id = each.value.subnet_ids
 
-  node_network_profile {
-    application_security_group_ids = [each.value.asg_id]
-  }
+#   node_network_profile {
+#     application_security_group_ids = [each.value.asg_id]
+#   }
 
-  tags = each.value.tags
-}
+#   tags = each.value.tags
+# }
 
+
+
+
+
+
+
+
+#################################################################################################
 # resource "azurerm_kubernetes_cluster_node_pool" "web" {
 #   name                    = "Web-NP"
 #   kubernetes_cluster_id   = azurerm_kubernetes_cluster.cluster.id
