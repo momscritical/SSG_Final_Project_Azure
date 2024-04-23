a1dff4a8eb8d8abe37adbb18fad3066fca9b160f
Taeyang <xodid3446@gmail.com>
2024-04-23 16:03:33 +0900

    Add Internal-LB

--- tae/output.tf
+++ tae/output.tf
@@ -6,16 +6,18 @@ output "public_ip_address" {
   value = azurerm_linux_virtual_machine.bastion.public_ip_address
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate
-  sensitive = true
-}
+# output "client_certificate" {
+#   value     = azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate
+#   sensitive = true
+# }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config_raw
-  sensitive = true
-}
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.cluster.kube_config_raw
+#   sensitive = true
+# }
 
+
+#################################################################################################
 # output "resource_group_name" {
 #     description = "The name of the created resource group."
 #     value = azurerm_resource_group.rg.name
