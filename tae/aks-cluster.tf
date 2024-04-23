00448659a59421dac454605ea9c70bd1151a352b
Taeyang <xodid3446@gmail.com>
2024-04-23 16:51:22 +0900

    240423

--- tae/aks-cluster.tf
+++ tae/aks-cluster.tf
@@ -3,7 +3,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   name                = "${var.project_name_prefix}-AKS-Cluster"
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "${var.project_name_prefix}-AKS-Cluster"
-  kubernetes_version =  "1.29"  
+  kubernetes_version =  "1.29"
 
   identity {
     type = "SystemAssigned"
