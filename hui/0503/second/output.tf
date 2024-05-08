c7b6de9a1e5b932189d018b44e2a36830cd474ba
HuijeWoo <152831616+HuijeWoo@users.noreply.github.com>
2024-05-08 12:40:57 +0900

    Delete hui/0503 directory
    oh

--- hui/0503/second/output.tf
+++
@@ -1,8 +0,0 @@
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-  sensitive = true
-}
-
-output "azure_oidc_url" {
-  value = azurerm_kubernetes_cluster.aks_cluster.oidc_issuer_url
-}
