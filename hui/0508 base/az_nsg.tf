468d953cbec6d2cf82b3018939474f217ddf9eb3
HuijeWoo <152831616+HuijeWoo@users.noreply.github.com>
2024-05-13 13:36:47 +0900

    Delete hui/0508 base directory
    delete

--- hui/0508 base/az_nsg.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_network_security_group" "nsg" {
-    name                = "${var.az_prefix}_nsg"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    lifecycle {
-        create_before_destroy = true
-    }
-}
